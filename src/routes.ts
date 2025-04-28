ac7b55e6a68a5cca4788a1a16c777bad960b50ff
gato1706 <thiago.gato17g@gmail.com>
2025-04-28 17:33:59 -0300

    definindo as fontes do relatorio

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,26 @@
 import { Router, Request, Response } from 'express'
 import { prismaClient } from './database/prismaClient'
 import { randomUUID } from 'node:crypto';
+import PdfPrinter from 'pdfmake';
 
 const routes = Router()
 
 routes.get('/products', async (request: Request, response: Response) => {
     const products = await prismaClient.products.findMany();
     return response.json(products)
+})
+
+routes.get("/products/report", (request: Request, response: Response) => {
+    const fonts = {
+        Helvetica: {
+            normal: 'Helvetica',
+            bold: 'Helvetica-Bold',
+            italics: 'Helvetica-Oblique',
+            bolditalics: 'Helvetica-BoldOblique'
+        }
+    };
+
+    const printer = new PdfPrinter(fonts);
 })
 
 routes.post('/products', async (req: Request, res: Response) => {
@@ -23,5 +37,7 @@ routes.post('/products', async (req: Request, res: Response) => {
 
     return res.status(201).json(createProduct)
 })
+
+
 
 export { routes }
